3c61ad39528220293a5d2695f65e28985af679ec
afeiship <1290657123@qq.com>
2019-11-15 11:11:13 +0800

    add: mv test->__tests__

--- __tests__/index.spec.js
+++ __tests__/index.spec.js
@@ -1,17 +1,14 @@
-(function () {
+(function() {
   var nx = require('next-js-core2');
   var NxBoilerplateClass = require('../src/next-boilerplate-class');
 
-  describe('NxBoilerplateClass.methods', function () {
-    test('init', function () {
+  describe('NxBoilerplateClass.methods', function() {
+    test('init', function() {
       var data = {
         key: 1,
         value: 2
       };
-      // console.log(rs1, rs2);
-
-      // expect(rs1).toBe(true);
-
+      expect(!!data).toBe(true);
     });
   });
-}());
+})();
