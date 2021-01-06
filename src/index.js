6860ddf3225954323678e7e90cac130c311c3beb
afeiship <1290657123@qq.com>
2021-01-06 09:18:17 +0800

    fix: global to ctx

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 (function() {
-  var global = window || global || this || Function('return this')();
+  var global = global || this || window || Function('return this')();
   var nx = global.nx || require('@jswork/next');
 
   var NxBoilerplateClass = nx.declare('nx.BoilerplateClass', {});
