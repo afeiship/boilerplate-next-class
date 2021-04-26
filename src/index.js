ca4cc7187bea81dd5df1a0655472e52d367b6b21
afeiship <1290657123@qq.com>
2021-04-26 22:40:05 +0800

    feat: defaults for iniital

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 (function() {
   var global = typeof window !== 'undefined' ? window : this || Function('return this')();
   var nx = global.nx || require('@jswork/next');
+  var defaults = { context: global };
 
-  var NxBoilerplateClass = nx.declare('nx.BoilerplateClass', {});
+  var NxBoilerplateClass = nx.declare('nx.BoilerplateClass', {
+    methods: {
+      init: function (inOptions) {
+        this.options = nx.mix(null, defaults, inOptions);
+      }
+    }
+  });
 
   if (typeof module !== 'undefined' && module.exports) {
     module.exports = NxBoilerplateClass;
